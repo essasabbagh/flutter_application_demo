6ffa14a25c7f6f207813671b904134e1068a028a
Essa Sabbagh <50638748+essasabbagh@users.noreply.github.com>
2024-06-07 09:54:41 +0300

    Add riverpod Observers

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get_storage/get_storage.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:flutter_application_demo/app.dart';
 
+import 'observers.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   await GetStorage.init();
 
-  runApp(const MainApp());
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.black,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
+
+  runApp(
+    ProviderScope(
+      observers: [
+        Observers(),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
